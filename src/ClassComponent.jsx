ce34ab35dd7d8dffddde4c0c61aaccad197329e3
AndreasLaptop <poniscript@gmail.com>
2023-05-24 18:56:55 +0200

    Some more lessons

---
+++ src/ClassComponent.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+//some example of class component
+class ClassComponent extends Component {
+    render() {
+        return <p>Text</p>;
+    }
+}
+
+export default ClassComponent;
---
+++ src/TryingProps.jsx
@@ -0,0 +1,9 @@
+function TryingProps({ text }) {
+    return <p>{text}</p>;
+}
+
+export default TryingProps;
+//Esta función devolvería un p
+//con el texto que le pasemos de esta forma :
+
+// <TryingProps text="Texto" />
